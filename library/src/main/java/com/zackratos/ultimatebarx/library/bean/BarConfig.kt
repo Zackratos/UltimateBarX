31f518a2f13610283d88ca0bf5638d7ad9955abd
Zackratos <869649338@qq.com>
2021-01-04 14:28:53 +0800

    delete deprecated code

--- library/src/main/java/com/zackratos/ultimatebarx/library/bean/BarConfig.kt
+++ library/src/main/java/com/zackratos/ultimatebarx/library/bean/BarConfig.kt
@@ -4,11 +4,6 @@ import android.graphics.Color
 import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import com.zackratos.ultimatebarx.library.UltimateBarX
-import com.zackratos.ultimatebarx.library.annotation.Type
-import com.zackratos.ultimatebarx.library.operator.OperatorProvider
 
 /**
  * @Author   : zhangwenchao
@@ -36,9 +31,6 @@ data class BarConfig(
      */
     internal var light: Boolean = false
 ) {
-
-    @Type
-    private var type: Int = UltimateBarX.STATUS_BAR
 
     companion object {
         fun newInstance(): BarConfig =
@@ -83,29 +75,6 @@ data class BarConfig(
             drawableRes = -1
         }
 
-    @Deprecated("")
-    private fun apply(activity: FragmentActivity) {
-        when (type) {
-            UltimateBarX.STATUS_BAR ->
-                OperatorProvider.create(activity).config(this).applyStatusBar()
-
-            UltimateBarX.NAVIGATION_BAR ->
-                OperatorProvider.create(activity).config(this).applyNavigationBar()
-
-        }
-    }
-
-    @Deprecated("")
-    private fun apply(fragment: Fragment) {
-        when (type) {
-            UltimateBarX.STATUS_BAR ->
-                OperatorProvider.create(fragment).config(this).applyStatusBar()
-
-            UltimateBarX.NAVIGATION_BAR ->
-                OperatorProvider.create(fragment).config(this).applyNavigationBar()
-        }
-    }
-
 
     internal fun update(config: BarConfig) {
         if (config == this) return
@@ -116,107 +85,4 @@ data class BarConfig(
         this.light = config.light
     }
 
-    @Deprecated("")
-    class Builder(@Type private val type: Int) {
-        private var fitWindow: Boolean = true
-        @ColorInt
-        private var color: Int = Int.MIN_VALUE
-        @DrawableRes
-        private var drawableRes: Int = -1
-        @ColorRes
-        private var colorRes: Int = -1
-        private var light: Boolean = false
-        @Deprecated("")
-        private var transparent: Boolean = false
-
-        companion object {
-            @Deprecated("")
-            fun newDefaultBuilder(@Type type: Int) =
-                Builder(type).apply {
-                    color = Int.MIN_VALUE
-                    colorRes = -1
-                    drawableRes = -1
-                    fitWindow = true
-                }
-        }
-
-        /**
-         *  use [com.zackratos.ultimatebarx.library.UltimateBarX.with]
-         *  and [com.zackratos.ultimatebarx.library.operator.Operator.applyStatusBar]
-         *  and [com.zackratos.ultimatebarx.library.operator.Operator.applyNavigationBar]
-         */
-        @Deprecated("")
-        fun apply(activity: FragmentActivity) {
-            build().apply(activity)
-        }
-
-        /**
-         *  use [com.zackratos.ultimatebarx.library.UltimateBarX.with]
-         *  and [com.zackratos.ultimatebarx.library.operator.Operator.applyStatusBar]
-         *  and [com.zackratos.ultimatebarx.library.operator.Operator.applyNavigationBar]
-         */
-        @Deprecated("")
-        fun apply(fragment: Fragment) {
-            build().apply(fragment)
-        }
-
-        @Deprecated("", ReplaceWith("colorInt(colorInt)"))
-        fun bgColor(@ColorInt colorInt: Int): Builder = color(colorInt)
-
-        @Deprecated("")
-        fun color(@ColorInt colorInt: Int): Builder =
-            apply {
-                if (transparent) return@apply
-                this.color = colorInt
-            }
-
-        @Deprecated("", ReplaceWith("colorRes(colorRes)"))
-        fun bgColorRes(@ColorRes colorRes: Int): Builder = colorRes(colorRes)
-
-        @Deprecated("")
-        fun colorRes(@ColorRes colorRes: Int): Builder =
-            apply {
-                if (transparent) return@apply
-                this.colorRes = colorRes
-            }
-
-        @Deprecated("")
-        fun fitWindow(fitWindow: Boolean): Builder = apply {
-            if (transparent) return@apply
-            this@Builder.fitWindow = fitWindow
-        }
-
-        @Deprecated("", ReplaceWith("drawableRes(drawableRes)"))
-        fun bgRes(@DrawableRes drawableRes: Int): Builder = drawableRes(drawableRes)
-
-        @Deprecated("")
-        fun drawableRes(@DrawableRes drawableRes: Int): Builder =
-            apply {
-                if (transparent) return@apply
-                this@Builder.drawableRes = drawableRes
-            }
-
-        @Deprecated("")
-        fun light(light: Boolean): Builder = apply { this@Builder.light = light }
-
-        @Deprecated("")
-        fun transparent(): Builder = apply {
-            transparent = true
-            fitWindow = false
-            color = Color.TRANSPARENT
-            colorRes = -1
-            drawableRes = -1
-        }
-
-        @Deprecated("")
-        internal fun build(): BarConfig =
-            BarConfig().apply {
-                type = this@Builder.type
-                fitWindow = this@Builder.fitWindow
-                color = this@Builder.color
-                colorRes = this@Builder.colorRes
-                drawableRes = this@Builder.drawableRes
-                light = this@Builder.light
-            }
-    }
 }
